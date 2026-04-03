d99672af5753b2529cfb36ac0cd1208620851fe8
Devanshu <devanshur10@gmail.com>
2026-04-03 23:34:30 +0530

    Updated file changes

--- app.js
+++ app.js
@@ -16,12 +16,13 @@ async function main() {
 }
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended: true}));
 
 app.get("/",(req,res) =>{
     res.send("hi,I am root");
 });
 
-
+// index route
 app.get("/listings", async (req, res) => {
     const allListings = await Listing.find({});
     
@@ -29,21 +30,37 @@ app.get("/listings", async (req, res) => {
     
     res.render("listings/index", { allListings });
 });
-
-app.get("/testListing",async (req,res) => {
-    let sampleListing = new Listing({
-        title:"My New Villa",
-        description: "By the beach",
-        price: 1200 ,
-        location: "calangute,Goa",
-        country: " India",
-    });
-
-    await sampleListing.save();
-    console.log("sample was saved");
-    res.send("successful testing");
-
+//new Route 
+app.get("/listings/new",(req,res) => {
+    res.render("listings/new.ejs");
 });
+
+// show route
+app.get("/listings/:id", async (req, res) =>{
+    let{id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs",{ listing });
+}) 
+//Create Route 
+app.post("/listings", async (req, res) => {
+    const newListing= new Listing( req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+// app.get("/testListing",async (req,res) => {
+//     let sampleListing = new Listing({
+//         title:"My New Villa",
+//         description: "By the beach",
+//         price: 1200 ,
+//         location: "calangute,Goa",
+//         country: " India",
+//     });
+
+//     await sampleListing.save();
+//     console.log("sample was saved");
+//     res.send("successful testing");
+
+// });
 
 app.listen(8080,() => {
     console.log("server is lsiting to port 8080")
